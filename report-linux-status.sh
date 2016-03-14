55bd3e39f3442204d7272ed943caacddc37c0399
Alexey <appplemac@gmail.com>
2016-03-14 18:19:36 -0400

    Add logging to bash script

--- report-linux-status.sh
+++ report-linux-status.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # relies on $LINUX_REPO and $LINUX_SHA1 being set
 
+set -x
+
 TEST_FAILURE_FILE="$LINUX_REPO/test-failure.txt"
 GITHUB_STATUS_URL="https://api.github.com/repos/appplemac/two-repo-workflow-linux/statuses/$LINUX_SHA1"
 
@@ -18,5 +20,5 @@ curl \
   --data "{ 'state': '$TEST_STATUS', \
            'target_url': '$CIRCLE_BUILD_URL', \
            'description': '$TEST_DESCRIPTION', \
-           'context': 'circleci/os-x' }"
+           'context': 'circleci/os-x' }" \
   --request POST "$GITHUB_STATUS_URL"
